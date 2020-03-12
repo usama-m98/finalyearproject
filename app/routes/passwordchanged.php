5711d5057792679aa6b90e63064544c4823d1418
usama <usama.musthafa98@gmail.com>
2020-03-12 18:32:20 +0000

    Added functionality to change passwords

--- app/routes/passwordchanged.php
+++ app/routes/passwordchanged.php
@@ -1,1 +1,70 @@
 <?php
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+$app->post('/passwordchanged', function(Request $request, Response $response) use ($app)
+{
+    $tainted = $request->getParsedBody();
+    $auth_info = getAuthInfo($app, $_SESSION['user']);
+    $is_password_same = isPasswordTheSame($tainted);
+    if($is_password_same)
+    {
+        $hashed_password = hash_password($app, $tainted['edit-password']);
+        $update_password = updatePasswordInDatabase($app, $hashed_password, $auth_info);
+
+        return $response->withRedirect('personaldetails');
+    }else{
+        return $response->withRedirect('editpassword');
+    }
+
+
+
+//    $html_output = $this->view->render($response,
+//        'result.html.twig',
+//        [
+//            'page_title' => 'Personal Details',
+//            'css_path' => CSS_PATH,
+//            'landing_page' => LANDING_PAGE,
+//            'js_path' => JS_PATH,
+//        ]);
+//
+//    processOutput($app, $html_output);
+//
+//    return $html_output;
+});
+
+function isPasswordTheSame($passwords)
+{
+    $is_password_same = false;
+    if($passwords['edit-password'] === $passwords['confirm-password'])
+    {
+        $is_password_same = true;
+    }
+
+    return $is_password_same;
+}
+
+function updatePasswordInDatabase($app, $password, $auth)
+{
+    $database_wrapper = $app->getContainer()->get('databaseWrapper');
+    $sql_queries = $app->getContainer()->get('dbQueries');
+    $settings = $app->getContainer()->get('settings');
+
+    $database_connection_settings = $settings['pdo_settings'];
+
+    $database_wrapper->setDatabaseConnectionSettings($database_connection_settings);
+    $database_wrapper->makeDatabaseConnection();
+
+    $query = $sql_queries->updatePassword();
+    $user_id = $auth['user_id'];
+
+    $parameters = [
+        ':password' => $password,
+        ':user_id' => $user_id
+    ];
+
+    $result = $database_wrapper->safeQuery($query, $parameters);
+
+    return $result;
+}
