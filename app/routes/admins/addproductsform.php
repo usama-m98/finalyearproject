6f38d4e8c053eda4f3613bba3f0472a624e23bb0
usama <usama.musthafa98@gmail.com>
2020-04-10 22:33:06 +0100

    completed form html for adding products

--- app/routes/admins/addproductsform.php
+++ app/routes/admins/addproductsform.php
@@ -5,6 +5,8 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 $app->get('/addproductsform', function (Request $request, Response $response) use ($app)
 {
+    $product_types = productTypes();
+
     if(isset($_SESSION['user'])) {
         if ($_SESSION['role'] == 'Admin' || $_SESSION['role'] == 'Root') {
             $html_output = $this->view->render($response,
@@ -15,12 +17,38 @@ $app->get('/addproductsform', function (Request $request, Response $response) us
                     'landing_page' => LANDING_PAGE,
                     'js_path' => JS_PATH,
                     'page_heading2' => 'Add Items',
-
+                    'action' => 'addproducts',
+                    'types' => $product_types
                 ]);
 
             processOutput($app, $html_output);
 
             return $html_output;
+        }else{
+            die();
         }
     }
 })->setName('addproductsform');
+
+function productTypes()
+{
+    $products = [
+        "Intel-Motherboard",
+        "AMD-Motherboard",
+        "Case",
+        "RAM",
+        "Power-Supply",
+        "Cooler",
+        "AMD-Processor",
+        "Intel-Processor",
+        "Storage",
+        "Graphics-Card",
+        "Laptops",
+        "Monitor",
+        "Desktops",
+        "Peripherals",
+        "Other"
+    ];
+
+    return $products;
+}
